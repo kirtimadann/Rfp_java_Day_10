be9575e738b1c8b3d9a6ec9a1053227e795c73a2
lunainfotec <kirtimadan1997@gmail.com>
2023-04-12 23:41:06 +0530

    add uC3

--- src/com/bl/addressbook/AddressBookMain.java
+++ src/com/bl/addressbook/AddressBookMain.java
@@ -1,52 +1,69 @@
 package com.bl.addressbook;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-         class NewContact{
+              class AddressBook  {
 
-             Scanner sc = new Scanner(System.in);
+                 public static void main(String[] args) {
 
-
-             String Firstname;
-             String Lastname;
-             String Address;
-             String city;
-
-             String email;
-
-             int pincode;
-             int phonenumber;
+                     AddressBook addressBook = new AddressBook();
 
 
+                     Scanner sc = new Scanner(System.in);
+                     int menu;
+                     String choice;
 
-         }
-public class AddressBookMain extends NewContact {
-    public static void main(String[] args) {
+                     System.out.println(" 0. Exit. ");
+                     System.out.println(" 1. Add contact. ");
+                     System.out.println(" 2. Edit contact. ");
+                     System.out.println(" 3. Outprint contact. ");
+                     System.out.println(" 4. Outprint all contacts. ");
+                     System.out.println(" 5. Delete contact. ");
+                     menu = sc.nextInt();
 
-        Scanner sc = new Scanner(System.in);
+                     while (menu != 0) {
 
-        //NewContact obj = new NewContact();
+                         switch (menu) {
 
-        System.out.println("Enter name");
-        String Firstname = sc.nextLine();
-        System.out.println("Enter last name");
-        String Lastname = sc.nextLine();
-        System.out.println("Enter Address");
-        String Address = sc.nextLine();
-        System.out.println("Enter city");
-        String city = sc.nextLine();
-        System.out.println("Enter email");
-        String email = sc.nextLine();
-        System.out.println("Enter pincode");
-        int pincode = sc.nextInt();
-        System.out.println("Enter phonenumber");
-        int phonenumber = sc.nextInt();
-        System.out.println(Firstname);
-        System.out.println(Lastname);
-        System.out.println(Address);
-        System.out.println(city);
-        System.out.println(email);
-        System.out.println(pincode);
-        System.out.println(phonenumber);
-    }
+                             case 1:
+                                 while (menu != 2) {
+                                     System.out.println("Enter First Name: ");
+                                     String firstName = sc.next();
+                                     System.out.println("Enter Last Name: ");
+                                     String lastName = sc.next();
+                                     System.out.println("Enter email: ");
+                                     String email = sc.next();
+                                      }
+                                 break;
 
-}
+                             case 2:
+                                 System.out.println("Enter First Name of contact that you would like to edit: ");
+                                 int index = sc.nextInt();
+
+                                 break;
+                             case 3:
+                                 System.out.println("Enter First Name of contact that you would like to delete: ");
+                                 choice = sc.next();
+
+                         }
+
+                         System.out.println(" 0. Exit. ");
+                         System.out.println(" 1. Add contact. ");
+                         System.out.println(" 2. Edit contact. ");
+                         System.out.println(" 3. Outprint contact. ");
+                         System.out.println(" 4. Outprint all contacts. ");
+                         System.out.println(" 5. Delete contact. ");
+                         menu = sc.nextInt();
+                     }
+                     System.out.println("Goodbye!");
+
+                 }
+
+                 private void addData(String firstName, String lastName, String email) {
+
+                 }
+             }
+
+
